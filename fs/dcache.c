6e0b61cc7755e28e0d5ffff50b6b4b55fcf2cf2d
Al Viro <viro@zeniv.linux.org.uk>
2014-10-26 19:19:16 -0400

    move d_rcu from overlapping d_child to overlapping d_alias
    commit 946e51f2bf37f1656916eb75bd0742ba33983c28 upstream.
    
    Change-Id: Id94e3ee1a68ce2a5345acc999798e55e6a8f2605
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: dianlujitao <dianlujitao@lineageos.org>

--- fs/dcache.c
+++ fs/dcache.c
@@ -3098,7 +3098,7 @@ void d_tmpfile(struct dentry *dentry, struct inode *inode)
 {
 	inode_dec_link_count(inode);
 	BUG_ON(dentry->d_name.name != dentry->d_iname ||
-		!hlist_unhashed(&dentry->d_alias) ||
+		!hlist_unhashed(&dentry->d_u.d_alias) ||
 		!d_unlinked(dentry));
 	spin_lock(&dentry->d_parent->d_lock);
 	spin_lock_nested(&dentry->d_lock, DENTRY_D_LOCK_NESTED);
