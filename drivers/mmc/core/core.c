0846486245f48754ccc3a9abb2926365d7f4dc29
Jaroslav Zvezda <acroreiser@gmail.com>
2019-02-08 22:28:53 +0200

    disable MMC SPI CRC

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -68,8 +68,10 @@ static const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
  * performance cost, and for other reasons may not always be desired.
  * So we allow it it to be disabled.
  */
-bool use_spi_crc = 0;
-module_param(use_spi_crc, bool, 0);
+int use_spi_crc = 0;
+EXPORT_SYMBOL(use_spi_crc);
+module_param_named(crc, use_spi_crc, int, 0644);
+MODULE_PARM_DESC(crc, "Enable/disable CRC");
 
 /*
  * We normally treat cards as removed during suspend if they are not
