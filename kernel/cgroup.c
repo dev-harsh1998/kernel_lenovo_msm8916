11b858f27249583df9f3ae574b8dfd3b433b3a42
Jaroslav Zviezda <acroreiser@gmail.com>
2020-04-03 17:21:55 +0300

    kernel: convert to kuid/kgid

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2212,9 +2212,9 @@ retry_find_task:
 
 	if (sysctl_iosched_boost_top_app == 1)
 	{
-		if (!memcmp(cgrp->name->name, "top-app", sizeof("top-app")) && tsk->cred->uid > 10000)
+		if (!memcmp(cgrp->name->name, "top-app", sizeof("top-app")) && tsk->cred->uid.val > 10000)
 			set_task_ioprio(tsk, IOPRIO_PRIO_VALUE(IOPRIO_CLASS_RT, 6));
-		else if ((!memcmp(cgrp->name->name, "background", sizeof("background")) || !memcmp(cgrp->name->name, "system-background", sizeof("system-background"))) && tsk->cred->uid > 10000)
+		else if ((!memcmp(cgrp->name->name, "background", sizeof("background")) || !memcmp(cgrp->name->name, "system-background", sizeof("system-background"))) && tsk->cred->uid.val > 10000)
 			set_task_ioprio(tsk, IOPRIO_PRIO_VALUE(IOPRIO_CLASS_IDLE, 0));
 		else
 			set_task_ioprio(tsk, IOPRIO_PRIO_VALUE(IOPRIO_CLASS_NONE, 0));
